b68a434c5dd881547bedac88fcb8e2028803c4b9
Alper Kaan <88709260+alperkaan01@users.noreply.github.com>
2024-05-22 02:15:00 +0300

    Add helper_functions for google cloud functions

--- chat-bot/helper_functions.py
+++ chat-bot/helper_functions.py
@@ -1,1 +1,43 @@
+import cv2
+from PIL import Image
+
+import random
+
+
+class colors:
+    RESET = "\033[0m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+
+
+def gaussian_blurring(img):
+    blurred_image = cv2.GaussianBlur(img, (15, 15), 0)
+    image = Image.fromarray(blurred_image)
+    #image.save("saved_image.png")
+    return blurred_image
+
+
+def convert_to_black_and_white(img):
+    bw_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    #cv2.imwrite("bw_image.png", bw_image)
+    return bw_image
+
+def convert_text_color(text):
+    options = [
+     colors.RED + text + colors.RESET,
+     colors.GREEN + text + colors.RESET,
+     colors.YELLOW + text + colors.RESET,
+     colors.BLUE + text + colors.RESET,
+     colors.MAGENTA + text + colors.RESET,
+     colors.CYAN + text + colors.RESET,
+     colors.WHITE + text + colors.RESET
+    ]
+
+    new_text = options[random.randint(0, len(options) - 1)]
+    return new_text
 
