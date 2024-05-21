7ee9bdcf064849724b61e9cafa088fe9ec389f22
Alper Kaan <88709260+alperkaan01@users.noreply.github.com>
2024-05-22 02:11:29 +0300

    Add Necessary Imports
    importing necessary modules for chat-bot in cloud functions

--- chat-bot/main.py
+++ chat-bot/main.py
@@ -1,1 +1,9 @@
+import functions_framework
 
+import base64
+
+import cv2
+import numpy as np
+from helper_functions import gaussian_blurring, convert_to_black_and_white, convert_text_color
+
+from encoder import encode_image_to_base64
