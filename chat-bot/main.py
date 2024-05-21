a918aef1c9fd80c0745e94f81b72178bef81be08
Alper Kaan <88709260+alperkaan01@users.noreply.github.com>
2024-05-22 02:40:47 +0300

    Delete unnecessary convert text color function

--- chat-bot/main.py
+++ chat-bot/main.py
@@ -4,7 +4,7 @@ import base64
 
 import cv2
 import numpy as np
-from helper_functions import gaussian_blurring, convert_to_black_and_white, convert_text_color, help_func
+from helper_functions import gaussian_blurring, convert_to_black_and_white, help_func
 
 from encoder import encode_image_to_base64
 
