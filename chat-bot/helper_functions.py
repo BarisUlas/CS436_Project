c676b2fe3b45fe438417d9835813323c347d9356
Alper Kaan <88709260+alperkaan01@users.noreply.github.com>
2024-05-22 02:41:14 +0300

    Delete unnecessary convert text color functions

--- chat-bot/helper_functions.py
+++ chat-bot/helper_functions.py
@@ -4,17 +4,6 @@ from PIL import Image
 import random
 
 
-class colors:
-    RESET = "\033[0m"
-    RED = "\033[31m"
-    GREEN = "\033[32m"
-    YELLOW = "\033[33m"
-    BLUE = "\033[34m"
-    MAGENTA = "\033[35m"
-    CYAN = "\033[36m"
-    WHITE = "\033[37m"
-
-
 def gaussian_blurring(img):
     blurred_image = cv2.GaussianBlur(img, (15, 15), 0)
     image = Image.fromarray(blurred_image)
@@ -29,18 +18,3 @@ def convert_to_black_and_white(img):
 
 def help_func():
   print("There are 2 image processing options you can apply to your image:\nOption 1: You can blur the image by selecting option 1 (Please write \"!bot blur\" for option 1)\nOption 2: You can convert the image background to black and white (Please write \"!bot bw\" for option 2)\nif you send a help command, this help documentation emerges :)\n")
-
-def convert_text_color(text):
-    options = [
-     colors.RED + text + colors.RESET,
-     colors.GREEN + text + colors.RESET,
-     colors.YELLOW + text + colors.RESET,
-     colors.BLUE + text + colors.RESET,
-     colors.MAGENTA + text + colors.RESET,
-     colors.CYAN + text + colors.RESET,
-     colors.WHITE + text + colors.RESET
-    ]
-
-    new_text = options[random.randint(0, len(options) - 1)]
-    return new_text
-
