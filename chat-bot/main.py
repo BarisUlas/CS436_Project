0a0f41b75fc972533dd7139c4a132f5b4e5b4f6d
Alper Kaan <88709260+alperkaan01@users.noreply.github.com>
2024-05-22 02:12:27 +0300

    Add Flask Framework For Chat Bot

--- chat-bot/main.py
+++ chat-bot/main.py
@@ -7,3 +7,28 @@ import numpy as np
 from helper_functions import gaussian_blurring, convert_to_black_and_white, convert_text_color
 
 from encoder import encode_image_to_base64
+
+@functions_framework.http
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    #print("Encoded String: ", encoded_string)
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+
+    return name #change this with encoded image
