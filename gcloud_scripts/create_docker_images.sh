1eab8868317734ad69f72557a8b29fd4609bc1a0
Barış Ulaş Çukur <barisc@sabanciuniv.edu>
2024-05-23 02:34:47 +0300

    added script that automatically creates docker images

---
+++ gcloud_scripts/create_docker_images.sh
@@ -0,0 +1,7 @@
+docker build ./express --tag express:local
+docker build ./socketio --tag socketio:local
+docker build ./client --tag client:local
+
+kind load docker-image express:local
+kind load docker-image socketio:local
+kind load docker-image client:local
