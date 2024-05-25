c820337209a25ea04acd661e95d6bb495060c26a
Barış Ulaş Çukur <barisc@sabanciuniv.edu>
2024-05-25 22:43:36 +0300

    almost done with chatbot

--- express/utils/chatBot.js
+++ express/utils/chatBot.js
@@ -1,12 +1,14 @@
 import axios from "axios";
+import { uploadToCloudStorage } from "./cloudStorage.js";
 
 const sendMessageToChatBot = async (action, image=null) => {
     let reqData = {action};
     if (action !== "help") {
         reqData.image = image.split(",")[1];
     }
-    const {data} =await axios.post("https://us-central1-alpine-guild-417209.cloudfunctions.net/chat-bot", reqData);
-    return data;
+    const { data } = await axios.post("https://us-central1-alpine-guild-417209.cloudfunctions.net/chat-bot", reqData);
+    const url = uploadToCloudStorage(data, "png");
+    return url;
 }
 
-export {sendMessageToChatBot}
+export { sendMessageToChatBot };
