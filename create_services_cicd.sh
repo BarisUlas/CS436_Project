97e01cf39e5fcd4566e253a0d3a1bbeacf9630ac
isiktan tanis <isiktantanis@isiktans-MacBook-Pro.local>
2024-05-25 19:58:35 +0300

    fix ci/cd

---
+++ create_services_cicd.sh
@@ -0,0 +1,5 @@
+kubectl delete -f ${{ env.K8S_DIR }}/ ./k8s/common/
+kubectl delete service nginx1 || true
+        
+kubectl apply -f ${{ env.K8S_DIR }}/ ./k8s/common/
+kubectl expose deployment nginx --name=nginx1 --type=LoadBalancer --port 80 --target-port 80
