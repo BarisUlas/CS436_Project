3a8cd0c562a79439e8da85748171ff7a95147aa8
Barış Ulaş Çukur <barisc@sabanciuniv.edu>
2024-05-26 00:27:26 +0300

    help

--- express/utils/chatBot.js
+++ express/utils/chatBot.js
@@ -7,7 +7,7 @@ const sendMessageToChatBot = async (action, image=null) => {
         reqData.image = image.split(",")[1];
     }
     const { data } = await axios.post("https://us-central1-alpine-guild-417209.cloudfunctions.net/chat-bot", reqData);
-    if (action === help) {
+    if (action === "help") {
         return data;
     }
     return await uploadToCloudStorage(data, "png");
